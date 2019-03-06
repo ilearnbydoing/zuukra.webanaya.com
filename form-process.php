c0c955ea4cb210fd715d79b686a2ff291593b670
ilearnbydoing <dgweb123@gmail.com>
2019-03-06 20:44:37 +0530

    first commit

---
+++ form-process.php
@@ -0,0 +1,43 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$company = $_POST["company"];
+$city = $_POST["city"];
+
+$EmailTo = "rajesh@risoninternational.co.in,durgesh@webanaya.com,info@hnhtwenty.com";
+$EmailFrom = "info@hnhtwenty.com";
+$Subject = "Zuukra - You have received New Enquiry";
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+
+$Body .= "Company: ";
+$Body .= $company;
+$Body .= "\n";
+
+$Body .= "City: ";
+$Body .= $city;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$EmailFrom);
+
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+
+?>
